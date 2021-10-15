635d7631a9abb9f2a9cc3f730a35ab49e8efe9dd
xTheCoderx <justaguyoney@gmail.com>
2021-10-15 18:15:54 +1300

    Added some code that detects the type of MotionEvent.
    For now I only have ACTION_UP. If the action is ACTION_UP the user will see a Toast.

--- app/src/main/java/com/realtomjoney/touchevents/MainActivity.kt
+++ app/src/main/java/com/realtomjoney/touchevents/MainActivity.kt
@@ -2,10 +2,35 @@ package com.realtomjoney.touchevents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val action = event!!.actionMasked
+
+        return when (action) {
+            MotionEvent.ACTION_DOWN -> {
+                true
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                true
+            }
+            MotionEvent.ACTION_MOVE -> {
+                true
+            }
+            MotionEvent.ACTION_UP -> {
+                Toast.makeText(this, "Touch is lifted", Toast.LENGTH_SHORT)
+                    .show()
+                true
+            }
+            else -> super.onTouchEvent(event)
+        }
     }
 }
